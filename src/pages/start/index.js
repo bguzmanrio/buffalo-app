92dd9f79af3631b3a0f9379f4d30cb09f3f52f4a
bguzmanrio <bguzmanrio91@gmail.com>
2018-12-24 01:20:17 +0100

    Feature toggle for config screen

--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StatusBar, Text, View, Button } from 'react-native';
+import Config from '../../../config';
 
 class Start extends React.Component {
   handleConfigNavigation = () => {
@@ -15,7 +16,9 @@ class Start extends React.Component {
       <View>
         <StatusBar hidden />
         <Text>This is the main page</Text>
-        <Button title="Configuration" onPress={this.handleConfigNavigation} />
+        {Config.featureToggle.configMenu && (
+          <Button title="Configuration" onPress={this.handleConfigNavigation} />
+        )}
         <Button title="Game" onPress={this.handleGameNavigation} />
       </View>
     );
